95e16cd1444b62df012f9ad48b60a71ef1bed4b1
vexus2 <rinrin900@gmail.com>
2015-04-10 19:21:51 +0900

    add bookmarks

--- config/routes.php
+++ config/routes.php
@@ -42,9 +42,20 @@ use Cake\Routing\Router;
 Router::defaultRouteClass('Route');
 
 Router::scope('/', function ($routes) {
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-    $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
-    $routes->fallbacks('InflectedRoute');
+  $routes->connect('/', [
+    'controller' => 'Pages',
+    'action'     => 'display',
+    'home'
+  ]);
+  $routes->connect('/pages/*', [
+    'controller' => 'Pages',
+    'action'     => 'display'
+  ]);
+  $routes->fallbacks('InflectedRoute');
+});
+
+Router::scope('/bookmarks', ['controller' => 'Bookmarks'], function ($routes) {
+  $routes->connect('/tagged/*', ['action' => 'tags']);
 });
 
 /**
